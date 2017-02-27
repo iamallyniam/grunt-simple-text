db91270e80fe06e9b4a3df11ce2ad957c8e2cd77
Allyn Thomas <allyn@iamallyniam.com>
2017-02-27 20:35:28 +0000

    changing to grunt reading of files

--- tasks/simple_text.js
+++ tasks/simple_text.js
@@ -8,43 +8,97 @@
 
 'use strict';
 
+var fs = require('fs');
+
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
+    
+    grunt.registerMultiTask('simple_text', 'Replace text with a key value from a json file', function() {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            json: '',
+            key: 'default',
+            openBracket : '{',
+            closeBracket : '}'
+        });
 
-  grunt.registerMultiTask('simple_text', 'Replace text with a key value from a json file', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+        var jsonFile = options.json;
+        var openBracket = options.openBracket;
+        var closeBracket = options.closeBracket;
+        var fileKey = options.key;
+        var regexp = new RegExp(openBracket + "(.*?)" + closeBracket,"g");
+        if(jsonFile){
+            
+            var jsonFileExists = grunt.file.exists(jsonFile);
+            
+            if(jsonFileExists){
+                
+                jsonData = grunt.file.readJSON(jsonFile);
+                jsonData = jsonData[fileKey];
+                
+                this.files.forEach(function (file) {
+                    var src = file.src[0];
+                    if (!src) {
+                        return;
+                    }
+                    
+                    var contents = grunt.file.read(src);
+                    var extract = contents.matchAll(regexp);
+                    
+                    if(extract){
+                        var extractLength = extract.length;
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+                        for(var i = 0; i < extractLength; i++){
+
+                            var matchedRule = extract[i][0];
+                            var matchedText = extract[i][1];
+                            
+                            
+
+                            contents = contents.replace(matchedRule, jsonData[matchedText]);
+
+                        }
+                        
+                        grunt.file.write(file.dest, contents);
+                        grunt.log.writeln('Classes removed from ' + src + ', and saved to ' + file.dest);
+                        
+                    }else{
+                        
+                        grunt.file.write(file.dest, contents);
+                        grunt.log.writeln('Classes removed from ' + src + ', and saved to ' + file.dest);
+                        
+                    }
+                    
+                });
+                
+                
+            }else{
+                grunt.log.writeln('Json file cannot be found.');
+            }
+            
+        }else{
+            grunt.log.writeln('No Json file set.');
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+        fs.readFile.stat()
+        
+        
     });
-  });
 
 };
+
+
+
+String.prototype.matchAll = function(regexp) {
+    var matches = [];
+    this.replace(regexp, function() {
+        var arr = ([]).slice.call(arguments, 0);
+        var extras = arr.splice(-2);
+        arr.index = extras[0];
+        arr.input = extras[1];
+        matches.push(arr);
+  });
+  return matches.length ? matches : null;
+};
+
